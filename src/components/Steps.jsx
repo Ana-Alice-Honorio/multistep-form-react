10e92add5b45c8671085dffeebe91e651fc822da
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2022-11-30 00:04:48 -0300

    create app

---
+++ src/components/Steps.jsx
@@ -0,0 +1,13 @@
+import "./Steps.css";
+
+const Steps = ({ currentSteps }) => {
+  return (
+    <div className="steps">
+      <div className="step">
+        <p>Identificação</p>
+      </div>
+    </div>
+  );
+};
+
+export default Steps;
